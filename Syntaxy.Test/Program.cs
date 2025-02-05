bd12ce2348d511f68f6b571063a231ab2d5bcd34
Moritz <101179677+mxritzdev@users.noreply.github.com>
2025-02-05 14:07:49 +0000

    added a part of the testing project

---
+++ Syntaxy.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Syntaxy;
+using Syntaxy.Languages;
+
+namespace Syntaxy.Test;
+
+public static class Program {
+
+    public static async Task Main(string[] args) 
+    {
+
+        var syntx = new SyntaxHighlighter()
+
+        syntx.Languages.Add(new CSharpLanguage());
+
+        var tokens = syntx.Parse()
+
+    }
+
+}
--- Syntaxy/Languages/CSharpLanguage.cs
+++ Syntaxy/Languages/CSharpLanguage.cs
@@ -23,68 +23,68 @@ public class CSharpLanguage : ILanguage
         var properties = new List<Property>();
 
         // Strings
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\"(.*?)\"",
             Type = TokenType.String
         });
 
         // Character literals
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "'(.*?)'",
             Type = TokenType.String
         });
         
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(public|private|protected|internal|static|readonly|const|sealed|virtual|override|abstract)\\b",
             Type = TokenType.Keyword
         });
         
         // Data Types
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(int|float|double|bool|string|void|decimal|byte|sbyte|short|ushort|long|ulong|char|object)\\b",
             Type = TokenType.Keyword
         });
         
         // Control Flow
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(if|else|for|foreach|while|do|switch|case|default|break|continue|goto|return)\\b",
             Type = TokenType.Keyword
         });
 
         // Access Modifiers
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(class|public|private|protected|internal|static|readonly|const|sealed|virtual|override|abstract)\\b",
             Type = TokenType.Identifier
         });
 
         // Exception Handling
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(try|catch|finally|throw)\\b",
             Type = TokenType.Keyword
         });
 
         // Contextual Keywords
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(async|await|yield|nameof|var|dynamic|partial)\\b",
             Type = TokenType.Keyword
         });
 
         // Null and Boolean Literals
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b(true|false|null)\\b",
             Type = TokenType.Keyword
         });
         
-        properties.Add(new Property()
+        properties.Add(new SimpleProperty()
         {
             Regex = "\\b-?\\b\\d+(\\.\\d+)?(f|F|d|d|u|U|m|M|l|L|ul|UL|lu|LU)?\\b",
             Type = TokenType.Number
